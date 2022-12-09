e73d27f82fd7914a3852caf215c7b90fb82d41df
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-12-09 17:40:31 -0600

    Initial Commit

---
+++ WidgetApp/WidgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetAppApp.swift
+//  WidgetApp
+//
+//  Created by user228199 on 12/9/22.
+//
+
+import SwiftUI
+
+@main
+struct WidgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
